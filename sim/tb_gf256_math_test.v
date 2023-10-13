da016bf4b5deeb275f045d65fbfc6c8121d0a9e4
Vladislav Knyazkov <vsknyaz@gmail.com>
2023-10-14 00:08:58 +0200

    Ready signal added

---
+++ sim/tb_gf256_math_test.v
@@ -0,0 +1,70 @@
+`timescale 1ns/100ps
+module tb;
+
+reg clk = 0;
+reg resb = 0;
+
+reg start = 0;
+reg [7:0] data = 0;
+
+always@(*) clk <= #10 ~clk;
+
+
+wire [7:0] w_data_inv;
+wire [7:0] w_mult_out;
+
+wire w_rdy;
+
+gf256_inv xi_gf256_inv(
+    .i_clk(clk),
+
+    .i_start(start),
+    .o_ready(w_rdy),
+
+    .x(data),
+    .y(w_data_inv)
+);
+
+gf256_mult xi_gf256_mult(
+    .A(data),
+    .B(w_data_inv),
+
+    .X(w_mult_out)
+);
+
+
+initial begin
+
+
+  $dumpfile("tb.vcd");
+  $dumpvars(0,tb);
+  $display("Starting RS syndrome calculator test");
+  #1000
+  resb = 1;
+
+  $display("Reset released");
+  #1000
+
+  data = 1;
+
+  
+  repeat (255) begin
+    @(posedge clk);
+    #1
+    start = 1;
+    @(posedge clk)
+    #1
+    data = data + 1;
+    start = 0;
+
+    @(posedge w_rdy);
+    #1
+    $display("x = %d, inv_x = %d, x*inv_x = %d", data, w_data_inv, w_mult_out);
+    
+  end
+
+  #1000
+  $finish;
+end
+
+endmodule
--- src/rs/gf256_inv.v
+++ src/rs/gf256_inv.v
@@ -5,42 +5,31 @@ module gf256_inv(
     input i_clk,
 
     input i_start,
-    
-    input [7:0] x,
-    output [7:0] y
-);
 
-/*
-wire [7:0] x2,x3,x4,x7,x8,x15,x30,x60,x120,x127,x254;	 
-	 
-gf256_mult MM1(.A(x),.B(x),.X(x2));
-gf256_mult MM2(.A(x),.B(x2),.X(x3));
-gf256_mult MM3(.A(x2),.B(x2),.X(x4));
-gf256_mult MM4(.A(x3),.B(x4),.X(x7));
-gf256_mult MM5(.A(x4),.B(x4),.X(x8));
-gf256_mult MM6(.A(x7),.B(x8),.X(x15));
-gf256_mult MM7(.A(x15),.B(x15),.X(x30));
-gf256_mult MM8(.A(x30),.B(x30),.X(x60));
-gf256_mult MM9(.A(x60),.B(x60),.X(x120));
-gf256_mult MM10(.A(x7),.B(x120),.X(x127));
-gf256_mult MM11(.A(x127),.B(x127),.X(x254));
-*/
+    input [7:0] x,
+    output [7:0] y,
+
+    output o_ready
+);
 
 reg [7:0] r_squares;
 reg [7:0] r_acc;
+reg [3:0] r_cntr;
 
 wire [7:0] w_m0_in, w_m0_out, w_m1_out;
 
 assign w_m0_in = i_start ? x : r_squares;
 gf256_mult xi_m0(.A(w_m0_in),.B(w_m0_in),.X(w_m0_out));
-gf256_mult xi_m2(.A(w_m0_out),.B(r_acc),.X(w_m1_out));
+gf256_mult xi_m2(.A(r_squares),.B(r_acc),.X(w_m1_out));
 
 always@(posedge i_clk)
 begin
     r_squares <= w_m0_out;
     r_acc <= i_start ? 8'h01 : w_m1_out;
+    r_cntr <= i_start | r_cntr === 7 ? 3'h0 : r_cntr + 1;
 end
 
 assign y = r_acc;
+assign o_ready = r_cntr===7; 
 
 endmodule
